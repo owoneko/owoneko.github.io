d3935d54535ced11678a032b61e566aeb9608876
owoneko <2784414167@qq.com>
2022-05-29 22:50:08 +0800

    modify boot.js

--- themes/fluid/source/js/boot.js
+++ themes/fluid/source/js/boot.js
@@ -1,13 +1,3 @@
-// ----- freecdn begin -----
-if (self.BigInt) {
-  self.FREECDN_SHARED_MODE = true;
-  self.FREECDN_PUBLIC_KEY = 'RVuXZ8vTj+cXxzVXpZyMJF10M6bV83vohq461axyRHTSdHDaUptc6RHDB4eV83UzN1c9c7q/oudnNPRr2nfC5w';
-  importScripts('/freecdn-internal/0.4.1/freecdn-main.min.js');
-} else {
-  console.warn('freecdn is not installed because the browser is too old');
-}
-// ----- freecdn end -----
-
 /* global Fluid */
 
 Fluid.boot = {};
