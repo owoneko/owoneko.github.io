db6617d6e34e86b09256ccab8dfa6483152e1621
owoneko <2784414167@qq.com>
2022-05-30 11:24:44 +0800

    modify boot.js

--- themes/fluid/source/js/boot.js
+++ themes/fluid/source/js/boot.js
@@ -1,3 +1,15 @@
+// ----- freecdn begin -----
+if (self.BigInt) {
+  self.FREECDN_SHARED_MODE = true;
+  self.FREECDN_PUBLIC_KEY = 'RVuXZ8vTj+cXxzVXpZyMJF10M6bV83vohq461axyRHTSdHDaUptc6RHDB4eV83UzN1c9c7q/oudnNPRr2nfC5w';
+  if( 'function' === typeof importScripts) {
+  importScripts('/freecdn-internal/0.4.1/freecdn-main.min.js');
+} else {
+  console.warn('freecdn is not installed because the browser is too old');
+}
+}
+// ----- freecdn end -----
+
 /* global Fluid */
 
 Fluid.boot = {};
