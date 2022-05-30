7309b991eb6ba376a237b383ab548a5c91ff6ebc
owoneko <2784414167@qq.com>
2022-05-30 19:18:53 +0800

    modify hexo-offline.config.cjs

--- hexo-offline.config.cjs
+++ hexo-offline.config.cjs
@@ -1,6 +1,6 @@
 // offline config passed to workbox-build.
 module.exports = {
     globPatterns: ["**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}"],
-    globDirectory: "/public",
-    swDest: "/themes/fluid/source/js/sw.js",
+    globDirectory: "C:/blog/public",
+    swDest: "/blog/themes/fluid/source/js/sw.js",
   }
